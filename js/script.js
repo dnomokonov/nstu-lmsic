f6821e469cae74343362d770195885822b51ea32
dnomokonov <zmnomokon715@gmail.com>
2024-05-26 17:04:34 +0700

    Active pages menu

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ let btnNavBar = document.querySelector('#btn')
 btnNavBar.onclick = function() {
     navbar.classList.toggle('active')
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let navbarLinks = document.querySelectorAll('.navbar ul li a');
+    let currentPath = window.location.pathname;
+
+    navbarLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('active');
+        }
+    });
+});
