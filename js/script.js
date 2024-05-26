10e58ecc5a20b75721633878ee3db19aa4b70d0e
dnomokonov <zmnomokon715@gmail.com>
2024-05-26 17:49:02 +0700

    debug active menu

--- js/script.js
+++ js/script.js
@@ -9,8 +9,12 @@ document.addEventListener("DOMContentLoaded", function() {
     let navbarLinks = document.querySelectorAll('.navbar ul li a');
     let currentPath = window.location.pathname;
 
+    console.log(currentPath)
+
     navbarLinks.forEach(link => {
         let linkPath = new URL(link.href, window.location.origin).pathname;
+
+        console.log(linkPath)
 
         if ((currentPath === '/' && linkPath === '/index.html') || linkPath === currentPath) {
             link.classList.add('active');
